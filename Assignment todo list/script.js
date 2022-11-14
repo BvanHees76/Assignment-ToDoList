4ebcf21381b63f6d003065e9a3913365e282934a
BvanHees76 <109236132+BvanHees76@users.noreply.github.com>
2022-11-14 20:12:58 +0100

    Add files via upload

---
+++ Assignment todo list/script.js
@@ -0,0 +1,91 @@
+/* Wat moet de user kunnen:
+1. Een task typen in het input veld
+2. wanneer hij klikt op de add-button moet de task zichtbaar
+zijn
+3. hij moet de task kunnen deleten met een prullenbakje wanneer
+deze afgerond is.
+​
+//event listener op add button - post request maken
+//event listener op add button - li toevoegen aan DOM*/
+let makeCompleteListItem = function (e) {
+    console.log(e);
+    const getTaskList = document.getElementById('task-list');
+    let makeLi = document.createElement('li');
+    const getInput = document.getElementById('write-task');
+    makeLi.innerHTML = getInput.value;
+    makeLi.className = `list-items-of-tasks ${e._id}`;
+   
+
+    let containerLiandBin = document.createElement('div');
+    containerLiandBin.setAttribute('class', 'div-li-bin');
+    let makeBinButton = document.createElement('input');
+    let makeBinButtonLabel = document.createElement('label');
+    makeBinButton.setAttribute('type', 'button');
+    makeBinButton.setAttribute('class', 'remove-bin');
+    makeBinButton.setAttribute('name', 'remove-task');
+    makeBinButtonLabel.setAttribute('for', 'remove-bin')
+    getTaskList.appendChild(containerLiandBin);
+    containerLiandBin.appendChild(makeLi);
+    containerLiandBin.appendChild(makeBinButton);
+    containerLiandBin.appendChild(makeBinButtonLabel);
+    }; 
+    //1. de taak die de user invult in het inputveld - moet de description zijn in het post-request
+
+    //hoe zorg ik ervoor dat input.value wordt overgenomen in de description?
+    //Zie POST function api-client.js
+    let storePostForEventlistener = function(){//omdat eventlistener niet op een async functie kan
+        let prom = postData();
+        prom.then(function(e) {
+            //ELKE KEER DAT JE DATA OPSLAAT KRIJG JE HET ALS REGEL TERUG. GEBRUIK HET IN JE UI
+            makeCompleteListItem(e);
+        });  
+    };
+
+    let clickPost = function()
+    {
+      const selAddButton = document.getElementById('add-task');
+      selAddButton.addEventListener('click', storePostForEventlistener);
+        //WERKT!!!!!!!!    
+    };
+
+   let addToDom = function()
+   {    
+     const selAddButton = document.getElementById('add-task');
+     selAddButton.addEventListener('click', makeCompleteListItem);
+   };
+
+   //DeLete
+   let deleteCompleteListItem = function()
+   {
+    let selBinButton = document.getElementsByClassName('remove-bin');
+    let parentDiv = selBinButton.parentNode;
+    parentDiv.remove();
+
+   };
+
+   let deleteFromDom = function()
+   {
+    let selBinButton = document.getElementsByClassName('remove-bin');
+    selBinButton.addEventListener('click', deleteCompleteListItem);
+    
+   };
+
+   //function delete item
+
+
+
+    let totalFunction = function()
+    {
+        //ZO HAAL JE DATA OP EN KAN JE ER DOORHEEN LUSSEN
+     let catcher = getData();
+        // console.log(catcher);
+        // catcher.then(function(result) {
+        //     for (let i = 0; i < result.length; i++) {
+        //         console.log(result[i]);
+        //     }
+        // });
+     clickPost();
+    };
+
+    totalFunction();
+    
